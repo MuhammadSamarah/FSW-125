dea4d5217abdc48746ee5aab6cf6e17f268f33ed
MuhammadSamarah <muhammad.samarah@bryanuniversity.edu>
2021-09-24 18:42:24 -0500

    update

--- Week3/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
-app.use("/bounties", require("./Week3/bountyRouter.js"))
-
-app.listen(9000, () => {
-    console.log("Server is Functioning Great!")
-})
